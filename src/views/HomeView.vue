fe7bb0ec714b1404ce84b9855ae05149c49c4de0
Orcan <tricyxyt@gmail.com>
2024-06-07 09:31:30 +0200

    Séparation des parties du code

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app class="background">
+    <Navbar></Navbar>
+    <Game class="game"></Game>
+    <Explain></Explain>
+  </v-app>
+</template>
+
+<script setup>
+import Navbar from '../components/NavbarComponent.vue'
+import Game from '../components/GameComponent.vue'
+import Explain from '../components/ExplainComponent.vue'
+</script>
+
+<style scoped>
+.background {
+  background-color: #309f6a;
+}
+.game {
+  margin-top: 8em;
+  margin-bottom: 10em;
+}
+</style>
